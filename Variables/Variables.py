87dd000665bbdc599fd33c8fe9cb550258c8b804
mona-git2020 <monalisha.prajapati@einfochips.com>
2020-09-03 22:49:47 +0530

    Created new three Test cases and make changes in

--- Variables/Variables.py
+++ Variables/Variables.py
@@ -1,2 +1,6 @@
 
 urlOFAmazon = "https://www.amazon.in/"
+
+brandsList = {"OnePlus" : "OnePlus", "Samsung" : "Samsung", "Boat" : "boAt", "Redmi" : "Redmi", "JBL" : "Jbl", "realme" : "realme", "OPPO" : "Oppo"}
+
+enterSearchProductName =	"OnePlus"
