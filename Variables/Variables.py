e827608a20a322b2acf800d092275ccb6d004a70
mona-git2020 <monalisha.prajapati@einfochips.com>
2020-09-28 20:14:02 +0530

    made changes in the test cases

--- Variables/Variables.py
+++ Variables/Variables.py
@@ -3,4 +3,6 @@ urlOFAmazon = "https://www.amazon.in/"
 
 brandsList = {"OnePlus" : "OnePlus", "Samsung" : "Samsung", "Boat" : "boAt", "Redmi" : "Redmi", "JBL" : "Jbl", "realme" : "realme", "OPPO" : "Oppo"}
 
-enterSearchProductName = ["OnePlus", "Lenovo Tab"]
+enterSearchProductName = ["OnePlus", "Lenovo Tab", "Realme 5"]
+
+extraSuggestions = ["in Electronics", "in Computers & Accessories"]
