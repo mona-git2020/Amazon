9eb10dcb835b240a7311333586ac59e831dc7ffd
Monalisha Prajapati <Monalisha.Prajapati@einfochips.com>
2020-11-26 20:09:39 +0530

    Made some changes in test cases

--- Variables/Variables.py
+++ Variables/Variables.py
@@ -1,6 +1,6 @@
 urlOFAmazon = "https://www.amazon.in/"
 
-brandsList = {"OnePlus" : "OnePlus", "Samsung" : "Samsung", "Boat" : "boAt", "Redmi" : "Redmi", "JBL" : "Jbl", "INFINITY" : "INFINITY", "Infinity" : "Infinity"}
+brandsList = {"OnePlus" : "OnePlus", "Samsung" : "Samsung", "Boat" : "boAt", "Redmi" : "Redmi", "Oppo" : "Oppo", "Infinity" : "Infinity", "INFINITY" : "INFINITY"}
 
 enterSearchProductName = ["OnePlus", "Lenovo Tab", "Realme"]
 
