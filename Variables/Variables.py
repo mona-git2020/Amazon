12536463a7d683f309b860a2eadb8e2e7d8c2d08
Monalisha Prajapati <Monalisha.Prajapati@einfochips.com>
2020-11-30 20:07:07 +0530

    Made some changes in amazon tasks for optimize the code.

--- Variables/Variables.py
+++ Variables/Variables.py
@@ -1,6 +1,6 @@
 urlOFAmazon = "https://www.amazon.in/"
 
-brandsList = {"OnePlus" : "OnePlus", "Samsung" : "Samsung", "Boat" : "boAt", "Redmi" : "Redmi", "Oppo" : "Oppo", "Infinity" : "Infinity", "INFINITY" : "INFINITY"}
+brandsList = {"OnePlus" : "OnePlus", "Samsung" : "Samsung", "Boat" : "boAt", "Redmi" : "Redmi", "JBL" : "Jbl", "realme" : "realme", "MI" : "Mi"}
 
 enterSearchProductName = ["OnePlus", "Lenovo Tab", "Realme"]
 
